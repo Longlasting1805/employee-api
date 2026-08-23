cf428046e82384d3c53831438a947b1be02de41a
Longlasting1805 <longlasting1805@gmail.com>
2026-08-23 12:05:46 +0100

    fix OpenAPI configuration

--- src/main/java/com/akande/employee_api/config/OpenApiConfig.java
+++ src/main/java/com/akande/employee_api/config/OpenApiConfig.java
@@ -2,7 +2,6 @@ package com.akande.employee_api.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -11,31 +10,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-        @Bean
-        public OpenAPI customOpenAPI() {
+    @Bean
+    public OpenAPI customOpenAPI() {
 
-                return new OpenAPI()
-                        .servers(
-                                java.util.List.of(
-                                        new Server()
-                                                .url("https://employeeapi.duckdns.org")
-                                                .description("Production server")
+        return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("Bearer Authentication")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "Bearer Authentication",
+                                        new SecurityScheme()
+                                                .name("Bearer Authentication")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
                                 )
-                        )
-                        .addSecurityItem(
-                                new SecurityRequirement()
-                                        .addList("Bearer Authentication")
-                        )
-                        .components(
-                                new Components()
-                                        .addSecuritySchemes(
-                                                "Bearer Authentication",
-                                                new SecurityScheme()
-                                                        .name("Bearer Authentication")
-                                                        .type(SecurityScheme.Type.HTTP)
-                                                        .scheme("bearer")
-                                                        .bearerFormat("JWT")
-                                        )
-                        );
-        }
+                );
+    }
 }
